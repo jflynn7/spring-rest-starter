0c5843202798fee24322bcbdfc25ea761f94876c
Flynn <jflynn@deloitte.co.uk>
2018-03-12 17:21:55 +0000

    Adds random account generator to CommandLineRunner

--- src/main/java/com/payment/solution/SolutionApplication.java
+++ src/main/java/com/payment/solution/SolutionApplication.java
@@ -1,7 +1,19 @@
 package com.payment.solution;
 
+import com.payment.solution.enums.AccountType;
+import com.payment.solution.model.Account;
+import com.payment.solution.repositories.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class SolutionApplication {
@@ -9,4 +21,43 @@ public class SolutionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SolutionApplication.class, args);
 	}
+
+    /**
+     * Adds CommandLineRunner to populate accounts table on boot
+     * @param repository
+     * @return
+     */
+	@Bean
+	public CommandLineRunner bootAccounts(AccountRepository repository) {
+        return (args) -> {
+            for(int x = 0; x < 20; x++) {
+                final Account account = new Account();
+                account.setAccountNumber(10000000 + x);
+                account.setAccountType(AccountType.CASH);
+                if( x < 10 ) {
+                    account.setSortCode("00-00-0" + String.valueOf(x));
+                } else {
+                    account.setSortCode("00-00-" + String.valueOf(x));
+                }
+                Random r = new Random();
+                account.setCurrentBalance(Math.round((10 + (1000 - 10) * r.nextDouble()) * 100d) / 100d);
+                repository.save(account);
+            }
+
+        };
+    }
+
+    /**
+     * Add CORS configuration, because, CORS is a bitch.
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/rest/").allowedOrigins("*");
+            }
+        };
+    }
 }
--- src/main/java/com/payment/solution/controllers/PaymentController.java
+++ src/main/java/com/payment/solution/controllers/PaymentController.java
@@ -1,5 +1,7 @@
 package com.payment.solution.controllers;
 
+import com.payment.solution.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PaymentController {
 
+    @Autowired
+    AccountRepository accountRepository;
+
     @RequestMapping("/rest/payments")
     public ResponseEntity makePayment() {
 
         return ResponseEntity.ok().body("Endpoint is alive!");
+    }
+
+    @RequestMapping("/rest/accounts")
+    public ResponseEntity getAccounts() {
+
+        return ResponseEntity.ok().body(accountRepository.findAll());
     }
 }
